0e66e5880c4fa9ab459e7ba7168c704be07a1fe5
Sergio <mendez.sergio50@gmail.com>
2021-06-30 20:34:05 -0600

    Creación de Modelos

---
+++ MyStuff_Sergio/MyStuff_Sergio/Models/Currency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStuff_Sergio.Models
+{
+    public partial class Currency
+    {
+
+        public Currency()
+        {
+            Items = new HashSet<Item>();
+        }
+
+        public int CurrencyId { get; set; }
+        public string CurrencyName { get; set; }
+        public string CurrencySym { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+
+    }
+}
